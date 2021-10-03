7bdd36c2c2f2252ba989362f1532a979089310fe
matic277 <matic277@gmail.com>
2021-10-03 12:56:33 +0200

    Poor svg icons for btns impl

--- src/main/java/com/example/gav_fx/panes/TopPane.java
+++ src/main/java/com/example/gav_fx/panes/TopPane.java
@@ -7,7 +7,11 @@ import com.example.gav_fx.graph.Node;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.FillRule;
+import javafx.scene.shape.SVGPath;
 
 public class TopPane extends FlowPane {
     
@@ -23,7 +27,28 @@ public class TopPane extends FlowPane {
     }
     
     private void initRunAlgorithmButton() {
-        Button btn = new Button("run");
+        SVGPath runSvg = new SVGPath();
+        runSvg.setContent("M4,2L14,8L4,14Z");
+        runSvg.setFill(new Color(0.35,0.66,0.41, 1));
+        SVGPath pauseSvg = new SVGPath();
+        pauseSvg.setContent("M0,0L10,0L10,10L0,10L0,0Z");
+        pauseSvg.setFill(new Color(0.86,0.35,0.38, 1));
+        
+        Button btn = new Button();
+        //BackgroundImage backgroundImage = new BackgroundImage(new Image(getClass().getResource("play.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        //Background background = new Background(backgroundImage);
+        //btn.setBackground(background);
+        //btn.getStyleClass().add("icon-button-run");
+        btn.setGraphic(runSvg);
+    
+        btn.setPrefSize(30, 30);
+    
+        System.out.println(btn.getPrefWidth());
+        runSvg.setScaleX(2);
+        runSvg.setScaleY(2);
+        pauseSvg.setScaleX(2);
+        pauseSvg.setScaleY(2);
+        
         btn.setOnMouseClicked(event -> {
             // Thread safe atomic boolean flip
             // flip the value of PAUSE
@@ -37,6 +62,8 @@ public class TopPane extends FlowPane {
             synchronized (AlgorithmController.PAUSE_LOCK) {
                 AlgorithmController.PAUSE_LOCK.notify();
             }
+            
+            btn.setGraphic(AlgorithmController.PAUSE.get() ? runSvg : pauseSvg);
         });
         
         this.getChildren().add(btn);
@@ -83,8 +110,22 @@ public class TopPane extends FlowPane {
     
     //FlatSVGIcon importIcon = new FlatSVGIcon("icons/flatlaf/FileChooserUpFolderIcon.svg").derive(iconSize.width, iconSize.height);
     private void initImportButton() {
-        Button btn = new Button("import");
-        //btn.getStyleClass().add("icon-button");
+        SVGPath svg = new SVGPath();
+        svg.setContent(
+                "M2,3L5.5,3L7,5L9,5L9,9L13,9L13,5L14,5L14,13L2,13Z" +
+                "M12,4L12,8L10,8L10,4L8,4L11,1L14,4L12,4Z");
+        
+        Button btn = new Button();
+        btn.setPrefSize(30, 30);
+        //btn.getStyleClass().add("icon-button-import");
+        btn.setGraphic(svg);
+    
+        System.out.println(btn.getPrefWidth());
+        //btn.setScaleX(2);
+        //btn.setScaleY(2);
+        svg.setScaleX(2);
+        svg.setScaleY(2);
+        
         btn.setOnMouseClicked(event -> {
             if (ImportGraphPane.isOpened) return;
             Parent root = new ImportGraphPane(this);
